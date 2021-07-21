74968be3ca13739f9899bca4b1e511563ef3372c
harshil-270 <harshiltagadiya@gmail.com>
2021-07-21 20:42:33 +0530

    included path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const socketio = require('socket.io');
 const http = require('http');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
