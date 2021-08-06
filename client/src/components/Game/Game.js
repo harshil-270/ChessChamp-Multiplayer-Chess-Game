df3e9a42a298a4bd71cc82647ce2ba36576a8413
harshil-270 <harshiltagadiya@gmail.com>
2021-08-06 12:43:25 +0530

    fixed socket issue

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -10,7 +10,7 @@ import { Alert, Spinner, Table } from 'react-bootstrap';
 import UserContext from '../../context/UserContext';
 import chess_move_sound from '../../assets/chess_move_sound.mp3';
 
-// learn more about what is fen in chess from here
+// Learn more about what is fen in chess from here.
 // https://www.chess.com/terms/fen-chess
 
 /*
@@ -20,8 +20,12 @@ import chess_move_sound from '../../assets/chess_move_sound.mp3';
 -when any player moves the piece, after validating that move another socket event is emitted for the move.
 */
 
+let socket = io(URL);
+
 function Game(props) {
-    const [fen, setFen] = useState('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+    const [fen, setFen] = useState(
+        'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+    );
     const [moves, setMoves] = useState([]);
     const [turn, setTurn] = useState('');
     const [playerColor, setPlayerColor] = useState('');
@@ -31,7 +35,10 @@ function Game(props) {
     // when game ends a popup is used to show the status(Win, Lose, Draw)
     const [showPopUp, setShowPopUp] = useState(false);
 
-    const [opponentDetails, setOpponentDetails] = useState({ username: '', rating: 0 });
+    const [opponentDetails, setOpponentDetails] = useState({
+        username: '',
+        rating: 0,
+    });
 
     // when user hovers over a piece on board. to indicate all possible positions, this positions are given different styling.
     const [squareStyles, setSquareStyles] = useState({});
@@ -41,7 +48,6 @@ function Game(props) {
     const [isLobbyFull, setIsLobbyFull] = useState(false);
 
     let chess = new Chess(fen);
-    const socket = io(URL);
     const User = useContext(UserContext);
     const gameId = props.match.params.gameId;
     const signal = axios.CancelToken.source();
@@ -69,7 +75,11 @@ function Game(props) {
             }
             setShowPopUp(true);
             try {
-                await axios.post(`${URL}/g/gameStatus`, { winner: winner, gameId: gameId }, { cancelToken: signal.token });
+                await axios.post(
+                    `${URL}/g/gameStatus`,
+                    { winner: winner, gameId: gameId },
+                    { cancelToken: signal.token }
+                );
             } catch (error) {
                 if (axios.isCancel(error)) {
                     console.error('Error: ', error.message);
@@ -77,11 +87,30 @@ function Game(props) {
             }
         }
     };
-
     const updateScroll = () => {
         // if moves history doesn't fit in window then scroll down to latest move.
         var element = document.getElementById('moves-table-container');
         if (element) element.scrollTop = element.scrollHeight;
+    };
+
+    const fetchOpponentDetails = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            const res = await axios.get(`${URL}/g/getOpponentDetails`, {
+                headers: { token: token },
+                params: { gameId: gameId, color: playerColorRef.current },
+            });
+            if (res.data.bothPlayerJoined) {
+                setOpponentDetails({
+                    username: res.data.username,
+                    rating: res.data.rating,
+                });
+            }
+        } catch (error) {
+            if (axios.isCancel(error)) {
+                console.error('Error: ', error.message);
+            }
+        }
     };
 
     useEffect(() => {
@@ -94,11 +123,16 @@ function Game(props) {
 
     useEffect(() => {
         const JoinGame = async () => {
+            if (!socket) {
+                socket = io(URL);
+            }
             try {
                 // CHecking if user has logged in or not.
                 // if not then redirect user to login page.
                 const token = localStorage.getItem('token');
-                const user = await axios.post(`${URL}/u/verifyToken`, { token: token });
+                const user = await axios.post(`${URL}/u/verifyToken`, {
+                    token: token,
+                });
                 if (!user.data.isValid) {
                     setRedirectToLogin(true);
                     return;
@@ -120,7 +154,9 @@ function Game(props) {
                     setPlayerColor(game.data.playerColor);
 
                     // get current player's turn from fen notation.
-                    game.data.fen.split(' ')[1] === 'w' ? setTurn('white') : setTurn('black');
+                    game.data.fen.split(' ')[1] === 'w'
+                        ? setTurn('white')
+                        : setTurn('black');
                     // if match is over then find the game status.
                     if (game.data.winner !== '') {
                         if (game.data.winner === 'draw') {
@@ -131,6 +167,7 @@ function Game(props) {
                             setMatchStatus('You lose');
                         }
                     }
+                    await fetchOpponentDetails();
                     setisLoading(false);
 
                     // make the socket connection/
@@ -138,7 +175,10 @@ function Game(props) {
                     socket.on('receiveMove', (data) => {
                         // check if this move is of opponents or not.
                         if (data.playerColor !== playerColorRef.current) {
-                            const audioEl = document.getElementsByClassName('audio-element')[0];
+                            const audioEl =
+                                document.getElementsByClassName(
+                                    'audio-element'
+                                )[0];
                             audioEl.play();
 
                             chess.load(data.fen);
@@ -146,45 +186,37 @@ function Game(props) {
 
                             if (isMounted.current) {
                                 setFen(chess.fen());
-                                setMoves([...movesRef.current, { from: data.move.from, to: data.move.to }]);
-                                setInCheck(chess.in_check() && !chess.in_checkmate());
+                                setMoves([
+                                    ...movesRef.current,
+                                    { from: data.move.from, to: data.move.to },
+                                ]);
+                                setInCheck(
+                                    chess.in_check() && !chess.in_checkmate()
+                                );
                                 setTurn(playerColorRef.current);
                             }
                         }
                     });
                     // whenever opponent joins set the  opponentDetail state.
                     socket.on('userJoined', async () => {
-                        try {
-                            const token = localStorage.getItem('token');
-                            const res = await axios.get(`${URL}/g/getOpponentDetails`, {
-                                headers: { token: token },
-                                params: { gameId: gameId, color: playerColorRef.current },
-                            });
-                            if (res.data.bothPlayerJoined) {
-                                setOpponentDetails({
-                                    username: res.data.username,
-                                    rating: res.data.rating,
-                                });
-                            }
-                        } catch (error) {
-                            if (axios.isCancel(error)) {
-                                console.error('Error: ', error.message);
-                            }
-                        }
+                        fetchOpponentDetails();
                     });
                 }
             } catch (error) {
-                if (axios.isCancel(error)) console.error('Error: ', error.message);
+                if (axios.isCancel(error))
+                    console.error('Error: ', error.message);
                 else setIsLobbyFull(true);
-
                 setisLoading(false);
             }
         };
         JoinGame();
 
         return () => {
-            socket.close();
             isMounted.current = false;
+            if (socket) {
+                socket.close();
+                socket = null;
+            }
             signal.cancel('Api calls are being cancelled');
         };
     }, []);
@@ -222,7 +254,11 @@ function Game(props) {
             }
 
             try {
-                await axios.post(`${URL}/g/postMove`, { gameId: gameId, move: move, fen: newFen }, { cancelToken: signal.token });
+                await axios.post(
+                    `${URL}/g/postMove`,
+                    { gameId: gameId, move: move, fen: newFen },
+                    { cancelToken: signal.token }
+                );
             } catch (error) {
                 if (axios.isCancel(error)) {
                     console.error('Error: ', error.message);
@@ -237,7 +273,8 @@ function Game(props) {
         const styling = {};
         for (let i = 0; i < moves.length; i++) {
             styling[moves[i].to] = {
-                background: 'radial-gradient(circle, #b5e48c 38%, transparent 40%)',
+                background:
+                    'radial-gradient(circle, #b5e48c 38%, transparent 40%)',
                 borderRadius: '50%',
             };
         }
@@ -253,39 +290,54 @@ function Game(props) {
             <Redirect
                 to={{
                     pathname: '/login',
-                    state: { from: props.location.pathname, message: 'Login to play the game' },
+                    state: {
+                        from: props.location.pathname,
+                        message: 'Login to play the game',
+                    },
                 }}
             />
         );
     }
     if (isLoading) {
         return (
-            <div className='Spinner'>
-                <Spinner animation='border' variant='primary' />
+            <div className="Spinner">
+                <Spinner animation="border" variant="primary" />
             </div>
         );
     }
     // if both player joined already in the game then displat lobby full message.
     if (isLobbyFull) {
-        return <Alert variant='warning'>Lobby is already full and game is started. Create new game to play with friends</Alert>;
+        return (
+            <Alert variant="warning">
+                Lobby is already full and game is started. Create new game to
+                play with friends
+            </Alert>
+        );
     }
 
     return (
-        <div id='GameContainer'>
-            <Modal show={showPopUp} backdrop='static' onHide={() => setShowPopUp(false)} keyboard={false}>
+        <div id="GameContainer">
+            <Modal
+                show={showPopUp}
+                backdrop="static"
+                onHide={() => setShowPopUp(false)}
+                keyboard={false}
+            >
                 <Modal.Header closeButton>
                     <Modal.Title>{matchStatus}</Modal.Title>
                 </Modal.Header>
             </Modal>
 
-            <div id='wrapper'>
-                <div id='left-sidebar-wrapper'>
-                    <Table striped bordered hover variant='dark'>
+            <div id="wrapper">
+                <div id="left-sidebar-wrapper">
+                    <Table striped bordered hover variant="dark">
                         <tbody>
                             {inCheck ? (
                                 <tr>
-                                    <td colSpan='2' id='in-check-text'>
-                                        {turn.charAt(0).toUpperCase() + turn.slice(1)} in Check
+                                    <td colSpan="2" id="in-check-text">
+                                        {turn.charAt(0).toUpperCase() +
+                                            turn.slice(1)}{' '}
+                                        in Check
                                     </td>
                                 </tr>
                             ) : (
@@ -304,31 +356,37 @@ function Game(props) {
                                 {matchStatus === 'Ongoing' ? (
                                     <td>{matchStatus}</td>
                                 ) : matchStatus === 'You Won' ? (
-                                    <td id='win-status'>You Won</td>
+                                    <td id="win-status">You Won</td>
                                 ) : (
-                                    <td id='lose-status'>You Lose</td>
+                                    <td id="lose-status">You Lose</td>
                                 )}
                             </tr>
                         </tbody>
                     </Table>
-                    <div id='game-link-container'>
+                    <div id="game-link-container">
                         <h6>Share this link to anyone to play</h6>
-                        <div id='game-link'>
-                            {window.location.href}{' '}
-                        </div>
+                        <textarea
+                            className="form-control"
+                            id="game-link"
+                            value={window.location.href}
+                            disabled={true}
+                        />
                         <button
-                            className='btn btn-light'
-                            id='copy-button'
+                            className="btn btn-light"
+                            id="copy-button"
                             onClick={() => {
-                                navigator.clipboard.writeText(window.location.href);
-                            }}>
+                                navigator.clipboard.writeText(
+                                    window.location.href
+                                );
+                            }}
+                        >
                             Copy link
                         </button>
                     </div>
                 </div>
 
-                <div id='chessboard-wrapper'>
-                    <div id='player-information-opponent'>
+                <div id="chessboard-wrapper">
+                    <div id="player-information-opponent">
                         {opponentDetails.username !== '' ? (
                             <>
                                 <div>Username: {opponentDetails.username}</div>
@@ -338,7 +396,7 @@ function Game(props) {
                             <div>Waiting for opponent to Join...</div>
                         )}
                     </div>
-                    <div id='chessboard'>
+                    <div id="chessboard">
                         <Chessboard
                             width={'550'}
                             position={fen}
@@ -359,18 +417,18 @@ function Game(props) {
                             // darkSquareStyle={{ backgroundColor: "CornFlowerBlue" }}
                         />
                     </div>
-                    <div id='player-information-you'>
+                    <div id="player-information-you">
                         <div>Username: {User.user.username}</div>
                         <div>Rating: {User.user.rating}</div>
                     </div>
                 </div>
 
-                <div id='right-sidebar-wrapper'>
-                    <div id='moves-table-container'>
-                        <Table striped bordered hover variant='dark'>
+                <div id="right-sidebar-wrapper">
+                    <div id="moves-table-container">
+                        <Table striped bordered hover variant="dark">
                             <thead>
                                 <tr>
-                                    <th colSpan='2' id='moves-header'>
+                                    <th colSpan="2" id="moves-header">
                                         Moves
                                     </th>
                                 </tr>
@@ -380,15 +438,15 @@ function Game(props) {
                                     if (index % 2 === 0) {
                                         return (
                                             <tr key={index}>
-                                                <td className='move-color-indicator-white'></td>
-                                                <td className='move-text'>{`${move.from} -> ${move.to}`}</td>
+                                                <td className="move-color-indicator-white"></td>
+                                                <td className="move-text">{`${move.from} -> ${move.to}`}</td>
                                             </tr>
                                         );
                                     } else {
                                         return (
                                             <tr key={index}>
-                                                <td className='move-color-indicator-black'></td>
-                                                <td className='move-text'>{`${move.from} -> ${move.to}`}</td>
+                                                <td className="move-color-indicator-black"></td>
+                                                <td className="move-text">{`${move.from} -> ${move.to}`}</td>
                                             </tr>
                                         );
                                     }
@@ -398,8 +456,8 @@ function Game(props) {
                     </div>
                 </div>
             </div>
-            <audio className='audio-element'>
-                <source type='audio/mp3' src={chess_move_sound}></source>
+            <audio className="audio-element">
+                <source type="audio/mp3" src={chess_move_sound}></source>
             </audio>
         </div>
     );
